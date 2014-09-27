9a6bf66e412a7f151bec9bc590edc27c36d36f7e
asugasawa <asugasawa@northwestel.net>
2014-09-27 00:18:57 -0700

    Yeoman
    App is developed with the aid of yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name openStatsBoardApp
+ * @description
+ * # openStatsBoardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('openStatsBoardApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openStatsBoardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openStatsBoardApp
+ */
+angular.module('openStatsBoardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
