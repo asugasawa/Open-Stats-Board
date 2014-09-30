ba0f35afc417cdaf10b6af28bdeea9d12d48558e
asugasawa <asugasawa@northwestel.net>
2014-09-29 23:44:14 -0700

    Partial work update

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,18 @@
  * # MainCtrl
  * Controller of the openStatsBoardApp
  */
-
-angular.module('mytodoApp')
+angular.module('openStatsBoardApp')
   .controller('MainCtrl', function ($scope) {
-      $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+     $scope.options1 = {
+                    renderer: 'area'
+                };
+                $scope.series1 = [{
+                        name: 'Series 1',
+                        color: 'steelblue',
+                        data: [{x: 0, y: 23}, {x: 1, y: 15}, {x: 2, y: 79}, {x: 3, y: 31}, {x: 4, y: 60}]
+                    }, {
+                        name: 'Series 2',
+                        color: 'lightblue',
+                        data: [{x: 0, y: 30}, {x: 1, y: 20}, {x: 2, y: 64}, {x: 3, y: 50}, {x: 4, y: 15}]
+                    }];
   });
